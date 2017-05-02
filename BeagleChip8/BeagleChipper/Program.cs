c6072d3755de84444adbb1418e2302971d9a0d45
CodingBeagle <kasper.rie@gmail.com>
2017-05-02 22:17:44 +0200

    Initial project commit (no, really, this time!)

---
+++ BeagleChip8/BeagleChipper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeagleChipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
